a979c90c5bd102b424bd7f4778d1722d3298588b
solomon-m <solomon.mark@anywhere.co>
2022-05-16 20:45:47 +0530

    drag&drop interacting with Dom

--- drag&drop/src/app.ts
+++ drag&drop/src/app.ts
@@ -3,6 +3,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement : HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement : HTMLInputElement;
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
@@ -10,7 +13,21 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input';
+
+        this.titleInputElement = document.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = document.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = document.querySelector('#people') as HTMLInputElement;
+        this.configure();
+
+
         this.attach()
+    }
+    private submiteHandler(event : Event){
+        event.preventDefault()
+        console.log("event -",this.titleInputElement.value);
+    }
+    private configure(){
+        this.element.addEventListener('submit',this.submiteHandler.bind(this));
     }
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
