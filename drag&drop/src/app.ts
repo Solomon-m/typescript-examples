92fb3662b990ad6a49615b18f4efbea84abafccc
solomon-m <solomon.mark@anywhere.co>
2022-05-16 22:14:35 +0530

    drag&drop

--- drag&drop/src/app.ts
+++ drag&drop/src/app.ts
@@ -3,9 +3,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
-    titleInputElement : HTMLInputElement;
+    titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
-    peopleInputElement : HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
@@ -20,12 +20,12 @@ class ProjectInput {
         this.configure();
         this.attach();
     }
-    private submiteHandler(event : Event){
+    private submiteHandler(event: Event) {
         event.preventDefault()
-        console.log("event -",this.titleInputElement.value);
+        console.log("event -", this.titleInputElement.value);
     }
-    private configure(){
-        this.element.addEventListener('submit',this.submiteHandler.bind(this));
+    private configure() {
+        this.element.addEventListener('submit', this.submiteHandler.bind(this));
     }
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
