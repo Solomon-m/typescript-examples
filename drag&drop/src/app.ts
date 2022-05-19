f9b2f496ae76f10dc15efdf9d921e207cf135cd8
solomon-m <solomon.mark@anywhere.co>
2022-05-19 23:59:56 +0530

    fixed projectState class issue.

--- drag&drop/src/app.ts
+++ drag&drop/src/app.ts
@@ -102,7 +102,13 @@ class ProjectList {
         this.element.querySelector('ul') !.id = listId;
         this.element.querySelector('h2') !.textContent = this.type.toUpperCase() + ' PROJECTS';
     }
-    
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`) !as HTMLUListElement;
+        listEl.innerHTML = '';
+        for (const prjItem of this.assignedProjects) {
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
+        }
+    }
 }
 class ProjectInput {
     templateElement: HTMLTemplateElement;
