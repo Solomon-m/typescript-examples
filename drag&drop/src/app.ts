0eb9ec70ce2d2289d616b1292c7363d3d1106ac6
solomon-m <solomon.mark@anywhere.co>
2022-05-21 23:47:10 +0530

    abstract component class

--- drag&drop/src/app.ts
+++ drag&drop/src/app.ts
@@ -110,15 +110,26 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor;
 }
 
-class Component {
-    constructor(public element: HTMLElement) {
-        this.element = element;
-        this.element.addEventListener('click', this.clickHandler);
+abstract class Component(T extends HTMLElement, U extends HTMLElement) {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+    constructor(templateId: string, hostElementId: string,insertAtStart: boolean ,newElementId ?: string){
+        this.templateElement = document.getElementById(templateId) as HTMLTemplateElement;
+        this.hostElement = document.getElementById(hostElementId) as T;
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as U;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.attach(insertAtStart);
     }
+    private attach(){
+        this.HTMLOListElement.insertAdjacentElement(insertAtStart ? 'afterbegin' : 'beforeend', this.element);
+    }
+    abstract configure(): void;
+    abstract renderContent(): void;
 
-    public clickHandler = (): void => {
-        console.log('I was clicked');
-    }
 }
 // ProjectList Class
 class ProjectList {
