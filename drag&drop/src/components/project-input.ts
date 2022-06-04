c4934617dd0915f05d67180aabd0b6ae6692511f
solomon-m <solomon.mark@anywhere.co>
2022-06-04 22:25:13 +0530

    default export and named export

--- drag&drop/src/components/project-input.ts
+++ drag&drop/src/components/project-input.ts
@@ -1,10 +1,10 @@
-import { Component } from './base-component.js';
+import Cmp  from './base-component.js';
 import * as Validation from '../util/validation.js';
-import { autobind } from '../decorators/autobind.js';
+import { autobind as Autobind } from '../decorators/autobind.js';
 import { projectState } from '../state/project-state.js';
 
 // ProjectInput Class
-export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
   peopleInputElement: HTMLInputElement;
@@ -68,7 +68,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     this.peopleInputElement.value = '';
   }
 
-  @autobind
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.gatherUserInput();
