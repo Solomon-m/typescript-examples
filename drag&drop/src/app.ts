59031dd3eb0c1fb43deceb199c965de887c6f063
solomon-m <solomon.mark@anywhere.co>
2022-06-05 23:46:09 +0530

    using lodash

--- drag&drop/src/app.ts
+++ drag&drop/src/app.ts
@@ -1,6 +1,9 @@
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
+import _ from "lodash";
 
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
+console.log(_.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
