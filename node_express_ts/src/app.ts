10dd65c8b63ff9df1d4486201938cf7232dc6267
solomon-m <solomon.mark@anywhere.co>
2022-06-10 23:41:14 +0530

    node_express_and_ts

---
+++ node_express_ts/src/app.ts
@@ -0,0 +1,10 @@
+import express,{Request,Response,NextFunction} from 'express';
+import  todoRouter  from './routes/todos';
+const app = express();
+app.use('/todos', todoRouter);
+app.use((error :Error,req : Request,res : Response,next : NextFunction)=>{
+    res.json({message:error.message});
+});
+app.listen(3000, () => {
+
+});
