ab869ccb68255f58a7ecd54940d0e70bb8fab578
solomon-m <solomon.mark@anywhere.co>
2022-06-10 22:00:47 +0530

    react todo app

--- reacttypescriptexample/src/App.tsx
+++ reacttypescriptexample/src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {Todo} from './todo.model';
 import TodoList from './components/TodoList';
-
+import NewTodo from './components/NewTodo';
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos,setTodos] = useState<Todo[]>([])
+  const todoAddHandler = (text: string) => {
+    console.log("text",text);
+    setTodos(prevTodos =>[...prevTodos,{id:Math.random().toString(),text:text}]);
+  };
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+  };
   return (
     <div className="App">
       {/* A component that adds todos */}
-      <TodoList items={todos} />
+      <NewTodo onAddTodo={todoAddHandler}/>
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
