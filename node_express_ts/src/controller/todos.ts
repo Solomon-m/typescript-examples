793fa5e16157efd539226a49d35057f71871c6dc
solomon-m <solomon.mark@anywhere.co>
2022-06-11 21:43:56 +0530

    final changes

--- node_express_ts/src/controller/todos.ts
+++ node_express_ts/src/controller/todos.ts
@@ -7,3 +7,28 @@ export const createTodo:RequestHandler = (req, res,next) => {
     TODOS.push(newTodo);
     res.status(201).json({message: 'Created the todo',createTodo:newTodo});
 };
+
+export const getTodos:RequestHandler = (req, res,next) => {
+    res.json({todos:TODOS});
+};
+
+export const updateTodos:RequestHandler<{id:string}> = (req, res,next) => {
+    const todoId = req.params.id;
+    const updatedText = (req.body as {text : string}).text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if(todoIndex < 0){
+        throw new Error('Could not find todo');
+    }
+    TODOS[todoIndex] = new Todo(TODOS[todoIndex].id,updatedText);
+    res.json({message: 'Updated the todo',todo:TODOS[todoIndex]});
+};
+
+export const deleteTodo:RequestHandler<{id:string}> = (req, res,next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if(todoIndex < 0){
+        throw new Error('Could not find todo');
+    }
+    TODOS.splice(todoIndex,1);
+    res.json({message: 'Deleted the todo'});
+};
